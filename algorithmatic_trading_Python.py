95f2070fdbbba54422ea8f121855d212dcee769e
Minghua(Mark) Li <markli@Marks-MBP.fios-router.home>
2017-10-01 20:12:12 -0400

    Update

--- algorithmatic_trading_Python.py
+++ algorithmatic_trading_Python.py
@@ -63,6 +63,9 @@ arr.argmin()
 np.random.seed(101)
 np.random.rand(1)
 
+# create 2D matrix (row,col,....) of random numbers
+np.random.randn(1000,2)
+
 
 arr/0 # will staill run and return warining
 '[Inf,Inf,....]'
@@ -276,7 +279,7 @@ axes2.set_title('SMALLER PLOT')
 
 # Multiple plot
 fig,axes = plt.subplots(nrow=1,ncol=2)
-plt.tight_layout() # adjust overlapping index
+plt.tight_layout() # adjust overlapping index (recommend always inclued by the end)
 
 axes[0].plot(x,y)
 axes[0].set_title('LARGER PLOT1')
@@ -285,12 +288,175 @@ axes[1].plot(y,x)
 axes[1].set_title('LARGER PLOT2')
 
 
-
-
-
-
 # >>> Figure Size and DPI
-fig = plt.figure(figsize=(3,2),dpi=100) # dpi - dots per pixel (usually use default)
+fig,axes = plt.subplots(nrows=1,ncols=2)
+axes
+'array of matplotlib.axes objects'
+
+# Signle plot
+fig = plt.figure(figsize=(3,2),dpi=100) # dot-per-inch (usually choose default) configure
+ax = fig.add_axes([0,0,1,1])
+ax.plot(x,y) 
+
+# multi-plot
+fig,axes = plt.subplots(nrows=2,ncols=1,figsize=(8,2))
+axes[0].plot(x,y)
+axes[1].plot(y,x)
+plt.tight_layout()
+
+# Save a fig
+fig.savefig('my_picture.jpg')
+fig.savefig('my_picture.png',dpi=200) # change pic setting
+
+# Add legend (two lines in one plot)
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+ax.plot(x,x**2,label='X Squared') # line 1
+ax.plot(x,x**3,label='X Cubed') # line 2
+ax.legend() 
+'or'
+ax.legend(loc=0) # 0,1,2 - check position
+'or'
+ax.legend(loc=(0.1,0.1)) # coordinates position
+
+
+
+# >>>> Plot Appearance
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+ax.plot(x,y,color='red') # red, green, blue, orange
+ax.plot(x,y,color='#FF8C00') # rRGB Hex code
+ax.plot(x,y,linewidth=1) # 1 width [lw]
+ax.plot(x,y,alpha=0.5) # transparency
+ax.plot(x,y,linestyle=':') # -- :,.,step,... [ls] 
+ax.plot(x,y,marker='o', # +,1,*,o,... data points
+            markersize=1, # dot size
+            markerfacecolor='yellow', # dot color
+            markeredgewidth=3, # dot circle edge width
+            markeredgecolor='green') # dot circle edge color 
+
+ax.set_xlim([0,1]) # x axis
+ax.set_ylim([0,1]) # y axis
+
+
+
+# other plot types
+plt.scatter(x,y)
+plt.hist(sample(range(1,1000),100))
+plt.boxplot([np.tandom.normal(0,std,100) for std in range(1,4)])
+'matplotlib plot types...'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ----------- Pandas Visualization + seanorn
+
+import numpy as np
+import pandas as pd
+import seaborn as sns # better plotting style (automatically apply)
+%matplotlib inline
+
+df1 = pd.read_csv('df1',index_col=0) # use first column as index (good for time series)
+
+# Plot with dataframe
+df1['col1'].hist(bins=30) # create hist gram in matplotlib plot
+'Or'
+df1['col1'].plot(kind='hist',bins=30) # create hist gram in matplotlib plot
+'or'
+df1['col1'].plot.hist(bins=30) # create hist gram in matplotlib plot
+
+
+df1.plot.area() # area plot for all columns (numeric columns)
+'or'
+df1.plot.area(alpha=0.4) # same plot but more transparency
+
+df1.plot.bar() # take index as categorical (horizontal) and bars for each columns across
+'or'
+df1.plot.bar(stacked=True) # same above by stacked into one bar for each index
+
+df1.plot.line(x=df1.index,y='col2') # line plot for column col2 and index as x-axis
+'or'
+df1.plot.line(x=df1.index,y='col2',figsize=(12,3),lw=3) # line plot with widder canvas, thicker line
+
+df1.plot.scatter(x='col1',y='col2',c='col3') # scatter plot of x,y and color coded on col3 value
+'or'
+df1.plot.scatter(x='col1',y='col2',c='col3',cmap='coolwarm') # same plot, red-blue color scale
+'or'
+df1.plot.scatter(x='col1',y='col2',s=fd1['col3']*100) # same plot, size (X100) coded by col3
+
+df1.plot.box() # boxplot for each of the numeric column (column names as horizontal)
+
+df1.plot.hexbin(x='col1',y='col2',gridsize=25) # like scatter plot but in hexigons 
+'or'
+df1.plot.hexbin(x='col1',y='col2',gridsize=25,cmap='coolwarm') # same above, red-blue
+
+df1['col'].plot.density() # smoothed, kernal density plot of a series
+'or'
+df1.plot.density() # density plot for all numeric columns
+
+
+
+# Visualizing time series -
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+mcdon = pd.read_csv('xxx.csv',index_col='Date',parse_dates=True) # read csv file, use 'Date' as index, parse_dates = time series
+
+# value columns at different scales (call series separately)
+mcdon['col1'].plot(figsize=(12,4)) # long canvas
+mcdon['col2'].plot(figsize=(12,4)) # long canvas
+
+# interested in sepacific date period
+mcdon['col2'].plot(xlim=['2007-02-01','2009-01-01'],ylim=(20,50)) # x,y limts reset to get clear view difference
+
+
+
+
+# ----- change date X-axis with custom plot
+# Extract information
+idx = mcdon.loc['2007-01-01':'2007-05-01'].index
+stock = mcdon.loc['2007-01-01':'2007-05-01']['col1']
+
+# Plotting
+fig,ax = plt.subplots()
+ax.plot_date(idx,stock,'-')
+
+# Major
+ax.xaxis.set_major_locator(dates.MonthLocator()) # identify month
+ax.xaxis.set_major_formatter(dates.DateFormatter('\n\n\n\n%b-%Y')) # Month format (can change)
+
+# Minor
+ax.xaxis.set_minor_locator(dates.WeekdayLocator(byweekday=0)) # 0 sun, 1 mon,... identify weekday
+ax.xaxis.set_minor_formatter(dates.DataFormatter('%d')) # weekday
+
+# adjust plotting
+fig.autofmt_xdate() # automatically adjust allignment
+plt.tight_layout()
+
+
+
+# ----- ONLY Jupitor notebook interactive plot
+'Restart the kernal - on top banner'
+
+'change' %matplotlib inline 'to' %matplotlib notebook
+
+df1.plot.line(x=df1.index,y='col2') 
+
+'Interactive features on plot bottom'
+
+
 
 
 
@@ -315,19 +481,54 @@ fig = plt.figure(figsize=(3,2),dpi=100) # dpi - dots per pixel (usually use defa
 'Data Source'
 ------------------------
 
+-1 'Pandas Data Reader' # allows connect to various API to pull data in as DF
+
+import pandas_datareader.data as web
+import datetime
+
+start = datetime.datetime(2015,1,1)
+end = datetime.datetime(2017,1,1)
+
+facebook = web.DataReader('FB','google',start,end) # company ticker, source api, start date, end date
+facebook.head()
+
+# Get Options data frame
+from pandas_datareader.data import Options
+fb_options = Options('FB','google')
+
+option_df = fb_options.get_options_data(expiry=fb_options.expiry_dates[0])
+option_df.head()
 
 
 
 
 
 
+-2 'Quandl' # offer robust Python API (also in R) free and paid data
 
+'quandl.com' # more data - alternative data + core financial data
+# Need to also register account 
 
+# test free version - 50 request / day
 
+- 'core financial data'
+- 'free check box'
+- 'select wiki prices'
+# should see 3000 US company stock pricess
 
+- 'On the right side, select Python table'
+- 'select date range on top'
+- 'Copy sample REQUEST code in Python'
 
+import quandl
+mydata = quandl.get('EIA/PET_RWTC_D') # oil price 
+mydata.head()
 
+mydata = quandl.get('WIKI/AAPL') # apple stock
+'or'
+mydata = quandl.get('WIKI/AAPL.1') # apple stock, only first column
 
+# find the code 'EIA/PET...' in web page "data" explore
 
 
 
